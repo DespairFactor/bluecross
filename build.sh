3ae8236b716466e65548c4439ec997bf7cb3a54f
DespairFactor <matthewalex@outlook.com>
2019-05-31 04:12:45 -0400

    build: V15R

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ export LD_LIBRARY_PATH=${HOME}/android/clang/clang-r353983c/lib64:$LD_LIBRARY_PA
 DEFCONFIG="b1c1_defconfig"
 
 # Kernel Details
-VER=".V14R"
+VER=".V15R"
 
 # Paths
 KERNEL_DIR=`pwd`
